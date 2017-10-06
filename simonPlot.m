d2a855c0d2449a4e8b651378ab60c5ceddaad063
Yago González <yagogonzalezg@gmail.com>
2017-10-06 21:13:55 +0200

    Refactor and unify loops

--- simonPlot.m
+++ simonPlot.m
@@ -3,22 +3,32 @@ function simonPlot(type, sett, export = false)
   
   rev = strcat("rev 1.1", strftime(" %Y/%m/%d - %R", localtime(time())));
 
+  nmin = sett(1);
+  nmax = sett(2);
+  rep = sett(3);
+
+  # Total right/wrong matrix
+  # Rows: R W
+  # Columns: different n values
+  trw = [];
+  # Total quantum steps matrix
+  # Rows: quantum steps for each repetition
+  # Columns: different n values
+  tqs = [];
+  # Total run times matrix
+  # Rows: times for each repetition
+  # Columns: different n values
+  trt = [];
+  for n = nmin:nmax
+    fprintf("Running for n = %i\n", n);
+    [rw, qsteps, times] = simon(n, rep);
+    trw = [trw; rw.'];  # The right/wrong count is 2x1
+    tqs = [tqs; qsteps];
+    trt = [trt; times];
+  endfor
+
   switch(type)
     case 1  # Right & wrong results / n
-      nmin = sett(1);
-      nmax = sett(2);
-      rep = sett(3);
-      
-      # Total right/wrong matrix
-      # Rows: R W
-      # Columns: different n values
-      trw = [];
-      for n = nmin:nmax
-        fprintf("Running for n = %i\n", n);
-        [rw, qsteps, time] = simon(n, rep);
-        trw = [trw; rw.'];
-      endfor
-      
       bar(nmin:nmax, trw);
       title(strcat("Right/Wrong results per value for n (", num2str(rep), " executions)"), "fontsize", 15, "fontweight", "bold");
       xlabel("n values");
@@ -26,20 +36,7 @@ function simonPlot(type, sett, export = false)
       legend("Right", "Wrong");
       
     case 2  # Average execution time / n
-      nmin = sett(1);
-      nmax = sett(2);
-      rep = sett(3);
       log = sett(4);
-      
-      # Total run times matrix
-      # Rows: times for each repetition
-      # Columns: different n values
-      trt = [];
-      for n = nmin:nmax
-        fprintf("Running for n = %i\n", n);
-        [rw, qsteps, times] = simon(n, rep);
-        trt = [trt; times];
-      endfor
       
       # Find the average times for each n
       avgts = mean(trt, 2);
@@ -55,21 +52,7 @@ function simonPlot(type, sett, export = false)
       xlabel("n values");
       ylabel("Classical time (s)");
     case 3 # Not used anymore. Kept for other graphs' ID number consistency
-    case 4  # Average quantum steps / n
-      nmin = sett(1);
-      nmax = sett(2);
-      rep = sett(3);
-
-      # Total quantum steps matrix
-      # Rows: quantum steps for each repetition
-      # Columns: different n values
-      tqs = [];
-      for n = nmin:nmax
-        fprintf("Running for n = %i\n", n);
-        [rw, qsteps, times] = simon(n, rep);
-        tqs = [tqs; qsteps];
-      endfor
-      
+    case 4  # Average quantum steps / n   
       # Find the average quantum steps for each n
       avgqs = mean(tqs, 2);
       
@@ -79,21 +62,7 @@ function simonPlot(type, sett, export = false)
       title(strcat("Average quantum steps per value for n (", num2str(rep), " executions)"), "fontsize", 15, "fontweight", "bold");
       xlabel("n values");
       ylabel("Number of quantum steps");
-    case 5  # Average quantums steps + classical big-O / n (logarithmic)
-      nmin = sett(1);
-      nmax = sett(2);
-      rep = sett(3);
-
-      # Total quantum steps matrix
-      # Rows: quantum steps for each repetition
-      # Columns: different n values
-      tqs = [];
-      for n = nmin:nmax
-        fprintf("Running for n = %i\n", n);
-        [rw, qsteps, times] = simon(n, rep);
-        tqs = [tqs; qsteps];
-      endfor
-      
+    case 5  # Average quantums steps + classical big-O / n (logarithmic)      
       # Find the average quantum steps for each n
       avgqs = mean(tqs, 2);
       
@@ -113,23 +82,7 @@ function simonPlot(type, sett, export = false)
       semilogy(nmin:nmax, os, "r", "markerfacecolor", "red");
       xlabel("n values");
       legend("Number of quantum steps", "Classical big-O notation (O(2n-1))");
-    case 6 # Average quantum steps + average execution time / n (logarithmic)
-      nmin = sett(1);
-      nmax = sett(2);
-      rep = sett(3);
-      
-      # Total run times matrix
-      # Rows: times for each repetition
-      # Columns: different n values
-      tqs = [];
-      trt = [];
-      for n = nmin:nmax
-        fprintf("Running for n = %i\n", n);
-        [rw, qsteps, times] = simon(n, rep);
-        tqs = [tqs; qsteps];
-        trt = [trt; times];
-      endfor
-      
+    case 6 # Average quantum steps + average execution time / n (logarithmic)      
       # Find the average quantum steps for each n
       avgqs = mean(tqs, 2);
       # Find the average times for each n
